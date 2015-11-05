cfdd76b34335e3ed82aa75589bcb95120a2beda4
219nate <nate.219@hotmail.com>
2015-11-05 21:46:03 +1000

    Starting Main code

--- GUI.py
+++ GUI.py
@@ -4,15 +4,32 @@ from kivy.lang import Builder
 from kivy.properties import StringProperty
 from kivy.properties import ListProperty
 from kivy.core.window import Window
+from currency import *
+from web_utility import *
+from trip import *
 
 Window.size = (300, 450)
 
-class Spinner(App):
-    TripCountryList = ["Belgium", "San Fransisco", "Austraia"]
+class App(App):
+    def __init__(self):
+        self.tripCountryList = ["Belgium", "San Fransisco", "Australia"]
+
+    def textBoxStatus(self, status=1):
+        if status == 1:
+            self.true.ids.HomeCountryAmount.disabled=True
+            self.true.ids.CurrentCountryAmount.disabled=True
+        else:
+            self.true.ids.HomeCountryAmount.disabled=False
+            self.true.ids.CurrentCountryAmount.disabled=False
 
     def build(self):
         self.title = "Foreign Exchange Calculator"
-        self.root = Builder.load_file('test_file.kv')
+        self.root = Builder.load_file('GUI.kv')
         return self.root
 
-Spinner().run()
+    def onPress(self):
+        print('buttonpress')
+        self.textBoxStatus(0)
+
+App().run()
+
